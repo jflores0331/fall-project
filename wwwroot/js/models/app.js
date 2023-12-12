0960888801bf99679396221398878d72068c9278
imon3y <152452276+imon3y@users.noreply.github.com>
2023-12-11 23:00:43 -0800

    Add files via upload
    I watched the video from Monday and this is what he did lmk if it works

---
+++ wwwroot/js/models/app.js
@@ -0,0 +1,77 @@
+﻿import { ShoppingList } from '../models/shopping-list.js'
+import mustache from './mustache.js';
+
+
+const demoTemplate = `
+
+{{#list}}
+    <p>
+    <strong>Name</strong> {{Name}}
+    <br>
+    <strong>Budget</strong {{{displayBudget}}}
+    </p>
+    <hr>
+{{#lists}}
+
+`;
+
+
+async function showShoppingList() { 
+
+    let url = "api/ShoppingList";
+
+    let request = await fetch(url);
+
+    let result = await request.json();
+
+    let shoppingLists = [];
+
+    result.forEach((settings) => {
+
+        shoppingLists.push(new ShoppingList(settings));
+
+    });
+
+    window.lastResult = result;
+
+    let context = {
+        list: []
+    };
+
+
+    context.lists = shoppingLists.sort((a, b) => {
+        return a.Name.localeCompare(b.Name)
+    });
+
+    window.currentContext = context; 
+
+    let html = mustache.render(demoTemplate, context);
+
+    renderShoppingList(html);
+}
+function renderShoppingList(html) { 
+
+    let target = document.querySelector("#ShoppingListArea");
+
+    target.innerHTML = html;
+}
+showShoppingList();
+
+window.sortShoppingListsByBudget = () => {
+     
+
+    const shoppingList = window.currentContext.lists.sort((a,b) => {
+        return a.Budget - b.Budget;
+
+    });
+
+    const newContext = {
+        lists: shoppingLists
+    };
+
+
+    let html = mustache.render(demoTemplate, newContext);
+
+    renderShoppingList(html); 
+}
+
---
+++ wwwroot/js/models/shopping-list.js
@@ -0,0 +1,32 @@
+﻿import { Item } from '../models/item.js';
+
+export class ShoppingList { 
+
+    constructor(settings) {
+
+        if (settings instanceof ShoppingList) {
+            return settings;
+        }
+
+        settings = settings ?? {};
+
+        this.Id = settings.Id ?? null;
+        this.Name = settings, Name ?? "No Name";
+        this.Budget = settings.Budget ?? 0;
+        this.Completed = settings.Completed ?? false;
+        this.Items = [];
+    }
+
+
+    addItem(settings) { 
+
+        const newItem = new Item(settings);
+
+        this.Items.push(newItem);
+    }
+
+    get displayBudget() { 
+        return `&dollar;${this.Budget.toFixed(2)}`;
+
+    }
+}
